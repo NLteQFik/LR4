7ccb30bc70c14c883f5bee5a23d71c9953734b68
HP <HP@DESKTOP-4TISIR1>
2025-04-12 12:09:56 +0300

    Добавьте файлы проекта.

---
+++ CargoCompany.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LR4
+{
+    internal class CargoCompany
+    {
+        private Tariff currentTariff;
+        private string companyName;
+        private double mass;
+
+        public double CurrentTariffPrice
+        {
+            get { return currentTariff.Price; }
+            set { currentTariff.Price = value; }
+        }
+
+        public string CompanyName
+        { 
+            get { return companyName; }
+            set { companyName = value; }
+        }
+
+        public double Mass
+        {
+            get { return mass; }
+            set { mass = value; }
+        }
+
+        private CargoCompany()
+        { 
+            currentTariff = new Tariff();
+            companyName = string.Empty;
+            mass = 0;
+        }
+
+        private CargoCompany(double price, string companyName, double mass)
+        {
+            currentTariff = new Tariff(price);
+            this.companyName = companyName;
+            this.mass = mass;
+        }
+
+        public static CargoCompany GetInstanceCargoCompany()
+        {
+            return new CargoCompany();
+        }
+
+        public static CargoCompany GetInstanceCargoCompany(double price, string companyName, double mass)
+        {
+            return new CargoCompany(price, companyName, mass);
+        }
+
+        public void increaseTariff(double amount)
+        {
+            currentTariff.increaseTariff(amount);
+        }
+
+        public void decreaseTariff(double amount)
+        {
+            currentTariff.decreaseTariff(amount);
+        }
+
+        public double calculateProfit()
+        {
+            return currentTariff.Price * mass;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using LR4;
+
+CargoCompany ex1 = CargoCompany.GetInstanceCargoCompany();
+CargoCompany ex2 = CargoCompany.GetInstanceCargoCompany(123.23, "RPS", 17);
+
+Console.WriteLine("Profit of company {0} is {1}", ex1.CompanyName, ex1.calculateProfit());
+Console.WriteLine("Profit of company {0} is {1}", ex2.CompanyName, ex2.calculateProfit());
+
+ex1.CompanyName = "Gruzoperevozki";
+ex1.Mass = 58.23;
+ex1.CurrentTariffPrice = 101.3;
+
+ex2.decreaseTariff(15.03);
+
+Console.WriteLine("Profit of company {0} is {1}", ex1.CompanyName, ex1.calculateProfit());
+Console.WriteLine("Profit of company {0} is {1}", ex2.CompanyName, ex2.calculateProfit());
